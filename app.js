df1a9ecd44ccad7c8da1c07feee9c3a9f95b1dc7
azotamiota <azotamiota@gmail.com>
2022-09-26 10:48:34 +0100

    add dotenv

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
@@ -28,6 +29,7 @@ app.post('/code', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000...')
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`)
 })
